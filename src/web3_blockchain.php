bb3172543e67f980615df2f4d22fe49e78ebc06a
GhostMeshIO <upwork.mc.landry@gmail.com>
2026-03-08 12:00:31 -0300

    Update web3_blockchain.php

--- src/web3_blockchain.php
+++ src/web3_blockchain.php
@@ -1,10 +1,14 @@
 <?php
-require 'vendor/autoload.php';
-use Web3\Web3;
+/**
+ * web3_blockchain.php – Blockchain anchoring (placeholder).
+ * This feature is not yet implemented. It will be added in a future release.
+ */
 
 function anchor_hash_on_chain($hash, $chain = 'sepolia') {
-    $web3 = new Web3('https://sepolia.infura.io/v3/YOUR_PROJECT_ID');
-    // Create and send transaction with hash as data
-    // ...
-    return $tx_hash;
+    error_log("Blockchain anchoring called but not implemented. Hash: $hash, chain: $chain");
+    return [
+        'success' => false,
+        'error' => 'Blockchain anchoring is not available in this version.',
+        'tx_hash' => null,
+    ];
 }
